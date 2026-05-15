238a1d99f5d021972004e7482cebfff0dfe657f9
jinvaramas <Jinvaramas@gamil.com>
2026-05-15 11:12:25 +0700

    mongoose

--- src/fakeData/fakeProducts.js
+++ src/fakeData/fakeProducts.js
@@ -1,5 +1,15 @@
 export const products = [
-  { id: "1", name: "Laptop", price: 35000, description: "High-performance laptop" },
+  {
+    id: "1",
+    name: "Laptop",
+    price: 35000,
+    description: "High-performance laptop",
+  },
   { id: "2", name: "Mouse", price: 500, description: "Wireless mouse" },
-  { id: "3", name: "Keyboard", price: 1200, description: "Mechanical keyboard" },
+  {
+    id: "3",
+    name: "Keyboard",
+    price: 1200,
+    description: "Mechanical keyboard",
+  },
 ];
