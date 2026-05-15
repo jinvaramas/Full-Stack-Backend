0ffa511485d4d8a942698cb894366074537cfd77
jinvaramas <Jinvaramas@gamil.com>
2026-05-15 11:24:42 +0700

    mongodbv2

--- src/routes/v2/index.js
+++ src/routes/v2/index.js
@@ -1,5 +1,8 @@
 import { Router } from "express";
+import { router as usersRoutes } from "./users.routes.js";
 
-const router = Router();
+export const router = Router();
 
-export default router;
+router.use("/users", usersRoutes);
+// router.use("/products", productsRoutes);
+// router.use("/notes", notesRoutes);
